28938195f321bb0b5738743181798e003ebd2840
tanakorn.ngam <tanakorn.ngamvittayachaisakool@bangkokbank.com>
2022-09-07 14:59:45 +0700

    prepare method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -20,6 +20,10 @@ namespace MyWebAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> GetProductByID(int id)
         {
+            if (id == 0)
+            {
+                return NotFound();
+            }
             return $"id {id}";
         }
 
@@ -28,6 +32,27 @@ namespace MyWebAPI.Controllers
         public ActionResult<string> SearchProduct([FromQuery]string name)
         {
             return $"name {name}";
+        }
+
+        //localhost/product
+        [HttpPost]
+        public ActionResult<string> AddProduct ()
+        {
+            return "post";
+        }
+
+        //localhost/product
+        [HttpPut]
+        public ActionResult<string> EditProduct()
+        {
+            return "put";
+        }
+
+        //localhost/product
+        [HttpDelete]
+        public ActionResult<string> DeleteProduct()
+        {
+            return "delete";
         }
     }
 }
