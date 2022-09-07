206ef60437c45092f09dfda6bc49ccfd8f6848a4
tanakorn.ngam <tanakorn.ngamvittayachaisakool@bangkokbank.com>
2022-09-07 13:54:59 +0700

    get request

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        //localhost/product
+        [HttpGet]
+        public ActionResult<string> GetProducts()
+        {
+            return "tanakorn";
+        }
+
+        //localhost/product/1234 => validation
+        //localhost/product/{id}
+        [HttpGet("{id}")]
+        public ActionResult<string> GetProductByID(int id)
+        {
+            return $"id {id}";
+        }
+
+        //localhost/product/search?name=123
+        [HttpGet("search")]
+        public ActionResult<string> SearchProduct([FromQuery]string name)
+        {
+            return $"name {name}";
+        }
+    }
+}
+
